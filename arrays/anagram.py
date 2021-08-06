9d7ea6d3bb2331ab6476d660e1215b6a828599d2
lee-gerald <lee.gerald.dev@gmail.com>
2021-08-06 14:14:10 -0600

    adjusted anagram

--- arrays/anagram.py
+++ arrays/anagram.py
@@ -1,20 +1,6 @@
 word_1 = "bonsor"
 word_2 = "robson"
-# is_anagram=[]
 
-# def anagram(string_1, string_2):
-#     for x in string_1:
-#         for y in string_2:
-#             if x == y:
-#                 is_anagram.append(x)
-#                 if len(is_anagram) == len(string_1) or len(is_anagram) == len(string_2):
-#                     print("anagram!")
-#                     return is_anagram
-
-# print(anagram(word_1, word_2))
-
-
-############ INSTRUCTOR SOLUTION ##########
 str1 = ['r','e','s','t','f','u','l']
 str2 = ['f','l','u','s','t','e','r']
 
@@ -32,3 +18,4 @@ def is_anagram(str1, str2):
     return True
 
 print(is_anagram(word_1, word_2))
+print(is_anagram(str1, str2))
